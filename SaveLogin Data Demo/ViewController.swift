9706895f6f5fd5e7d9c6dc62957f8bdcc6c83981
Hamdi <m.hamdi201833@gmail.com>
2023-02-14 09:03:46 -0800

    Initial Commit

---
+++ SaveLogin Data Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveLogin Data Demo
+//
+//  Created by hamdi on 14/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
